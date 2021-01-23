b9f8b82cd6ec0db5132bb905b84d42e8120bb266
Tom Durrant <tomchdurrant@gmail.com>
2021-01-23 22:30:58 +0000

    Add hook examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { createContext, useContext, useEffect, useState } from "react"
+
+
+const moods = {
+  happy: ':)',
+  sad: ':('
+}
+
+const MoodContext = createContext(moods)
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(0);
+
+  useEffect(() => {
+    /* Runs when the value in the array is updated
+    empty array means no dependencies */
+    alert('hello side effect')
+
+    /* Runs on tear down of the state/component */
+    return () => alert('goodbye')
+  }, [count])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCount(count + 1)}>
+          {count} 
+        </button>
+        <button onClick={() => setOtherCount(otherCount + 1)}>
+          {otherCount}
+        </button>
+        <h2>Context Demo</h2>
+        <MoodContext.Provider value={moods.happy}>
+          <MoodEmoji />
+        </MoodContext.Provider>
       </header>
     </div>
   );
+}
+
+function MoodEmoji() {
+  const mood = useContext(MoodContext);
+  return <p>{mood}</p>
 }
 
 export default App;
